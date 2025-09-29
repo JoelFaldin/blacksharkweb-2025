2714b39e2e0496e2d06433581a240dd4f0d54ded
SamuelSotomayor1 <andressotomayor477@gmail.com>
2025-09-29 20:20:09 -0300

    feat: structure of portfolio are updated

--- src/app/portafolio/page.tsx
+++ src/app/portafolio/page.tsx
@@ -1,8 +1,12 @@
 const Portafolio = () => {
   return (
-    <div>
-      This is the portfolio component
-    </div>
+    <>
+    <section className="w-full grid grid-rows-3 justify-center">
+      <div className="h-32">Fotografía Digital</div>
+      <div className="h-32">Diseño Gráfico</div>
+      <div className="h-32">Servicios Extra</div>
+    </section>
+    </>
   )
 }
 
